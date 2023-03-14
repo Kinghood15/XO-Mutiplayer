dcc3239e25775fea0a218ef7d8da124d90c3bc6c
kinghood15 <annguyen8278163@gmail.com>
2023-03-14 18:35:03 +0700

    update file http

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 dotenv.config({ path: './config.env' });
 
-const server = require('https').createServer(app);
+const server = require('http').createServer(app);
 
 const io = require('socket.io')(server, {
     cors: {
